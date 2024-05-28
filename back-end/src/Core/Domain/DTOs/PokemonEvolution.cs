ecddb16174bf59d0ef93b94aba903fb0236bbf70
Misael901218 <Misael.c440@gmail.com>
2024-05-27 20:57:12 -0500

    Se corrije estructura de la clase evolucion

--- back-end/src/Core/Domain/DTOs/PokemonEvolution.cs
+++ back-end/src/Core/Domain/DTOs/PokemonEvolution.cs
@@ -8,157 +8,144 @@ using System.Threading.Tasks;
 
 namespace Domain.DTOs
 {
-         public class Chain
+    public class Chain
     {
-        [JsonProperty("evolution_details")]
+        [JsonProperty("evolution_details", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("evolution_details")]
         public List<object> evolution_details { get; set; }
 
-        [JsonProperty("evolves_to")]
+        [JsonProperty("evolves_to", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("evolves_to")]
         public List<EvolvesTo> evolves_to { get; set; }
 
-        [JsonProperty("is_baby")]
+        [JsonProperty("is_baby", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("is_baby")]
-        public bool is_baby { get; set; }
+        public bool? is_baby { get; set; }
 
-        [JsonProperty("species")]
+        [JsonProperty("species", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("species")]
         public Species species { get; set; }
     }
 
     public class EvolutionDetail
     {
-        [JsonProperty("gender")]
+        [JsonProperty("gender", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("gender")]
         public object gender { get; set; }
 
-        [JsonProperty("held_item")]
+        [JsonProperty("held_item", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("held_item")]
         public object held_item { get; set; }
 
-        [JsonProperty("item")]
+        [JsonProperty("item", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("item")]
         public object item { get; set; }
 
-        [JsonProperty("known_move")]
+        [JsonProperty("known_move", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("known_move")]
         public object known_move { get; set; }
 
-        [JsonProperty("known_move_type")]
+        [JsonProperty("known_move_type", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("known_move_type")]
         public object known_move_type { get; set; }
 
-        [JsonProperty("location")]
+        [JsonProperty("location", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("location")]
         public object location { get; set; }
 
-        [JsonProperty("min_affection")]
+        [JsonProperty("min_affection", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("min_affection")]
         public object min_affection { get; set; }
 
-        [JsonProperty("min_beauty")]
+        [JsonProperty("min_beauty", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("min_beauty")]
         public object min_beauty { get; set; }
 
-        [JsonProperty("min_happiness")]
+        [JsonProperty("min_happiness", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("min_happiness")]
         public object min_happiness { get; set; }
 
-        [JsonProperty("min_level")]
+        [JsonProperty("min_level", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("min_level")]
-        public int min_level { get; set; }
+        public int? min_level { get; set; }
 
-        [JsonProperty("needs_overworld_rain")]
+        [JsonProperty("needs_overworld_rain", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("needs_overworld_rain")]
-        public bool needs_overworld_rain { get; set; }
+        public bool? needs_overworld_rain { get; set; }
 
-        [JsonProperty("party_species")]
+        [JsonProperty("party_species", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("party_species")]
         public object party_species { get; set; }
 
-        [JsonProperty("party_type")]
+        [JsonProperty("party_type", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("party_type")]
         public object party_type { get; set; }
 
-        [JsonProperty("relative_physical_stats")]
+        [JsonProperty("relative_physical_stats", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("relative_physical_stats")]
         public object relative_physical_stats { get; set; }
 
-        [JsonProperty("time_of_day")]
+        [JsonProperty("time_of_day", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("time_of_day")]
         public string time_of_day { get; set; }
 
-        [JsonProperty("trade_species")]
+        [JsonProperty("trade_species", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("trade_species")]
         public object trade_species { get; set; }
 
-        [JsonProperty("trigger")]
+        [JsonProperty("trigger", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("trigger")]
         public Trigger trigger { get; set; }
 
-        [JsonProperty("turn_upside_down")]
+        [JsonProperty("turn_upside_down", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("turn_upside_down")]
-        public bool turn_upside_down { get; set; }
+        public bool? turn_upside_down { get; set; }
     }
 
     public class EvolvesTo
     {
-        [JsonProperty("evolution_details")]
+        [JsonProperty("evolution_details", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("evolution_details")]
         public List<EvolutionDetail> evolution_details { get; set; }
 
-        [JsonProperty("evolves_to")]
+        [JsonProperty("evolves_to", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("evolves_to")]
         public List<EvolvesTo> evolves_to { get; set; }
 
-        [JsonProperty("is_baby")]
+        [JsonProperty("is_baby", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("is_baby")]
-        public bool is_baby { get; set; }
+        public bool? is_baby { get; set; }
 
-        [JsonProperty("species")]
+        [JsonProperty("species", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("species")]
         public Species species { get; set; }
     }
 
     public class PokemonEvolutionResponse
     {
-        [JsonProperty("baby_trigger_item")]
+        [JsonProperty("baby_trigger_item", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("baby_trigger_item")]
         public object baby_trigger_item { get; set; }
 
-        [JsonProperty("chain")]
+        [JsonProperty("chain", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("chain")]
         public Chain chain { get; set; }
 
-        [JsonProperty("id")]
+        [JsonProperty("id", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("id")]
-        public int id { get; set; }
-    }
-
-    public class Species
-    {
-        [JsonProperty("name")]
-        [JsonPropertyName("name")]
-        public string name { get; set; }
-
-        [JsonProperty("url")]
-        [JsonPropertyName("url")]
-        public string url { get; set; }
+        public int? id { get; set; }
     }
 
     public class Trigger
     {
-        [JsonProperty("name")]
+        [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("name")]
         public string name { get; set; }
 
-        [JsonProperty("url")]
+        [JsonProperty("url", NullValueHandling = NullValueHandling.Ignore)]
         [JsonPropertyName("url")]
         public string url { get; set; }
     }
-
-
 
 
 }
